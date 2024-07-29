f3f9225321e5af4f4491335d859ac9e9f3baf738
V4nden <vanden.gd@gmail.com>
2024-07-29 19:44:09 +0300

    fix: WHY THIS ISN'T WORKING

--- app/api/news/route.ts
+++ app/api/news/route.ts
@@ -1,10 +1,10 @@
-import autopost from "@/src/utils/autopost";
 import {
   createTicketPlay,
   getTicketPlayByDiscord,
   pb,
 } from "@/src/utils/pocketbase";
 import fs from "fs/promises";
+import moment from "moment";
 import { getServerSession } from "next-auth";
 export async function POST(req: Request) {
   const session = await getServerSession();
@@ -13,7 +13,56 @@ export async function POST(req: Request) {
     if (
       process.env.ADMIN_MAILS?.split(" ").includes(String(session.user.email))
     ) {
-      autopost(body.title, body.image, body.content);
+      const data = {
+        embeds: [
+          {
+            title: "",
+            footer: { text: "©Pixel " + moment().year() },
+            image: {
+              url: body.image,
+            },
+            description: `# ${body.title}
+      ${body.message}
+    
+              ||<@&1055112639245713408>||`,
+            color: 14727558, // Decimal color code
+          },
+        ],
+        username: "PNews",
+        avatar_url:
+          "https://cdn.discordapp.com/avatars/837341994962911313/867883fd8a0c25656c67adac2eb8be55.webp?size=32",
+      };
+      fetch(String(process.env.NDISCORD_TOKEN), {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      }).then(async (res) => {
+        console.log(await res.status);
+      });
+      fetch(
+        "https://api.telegram.org/bot" +
+          process.env.NTELEGRAM_TOKEN +
+          "/sendPhoto",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            chat_id: -1001555804470,
+            caption: `🧡 *${body.title}* \n\n${body.message}`,
+            photo: body.image,
+            parse_mode: "Markdown",
+          }),
+        }
+      ).then(async (res) => {
+        console.log(await res.json());
+      });
+      await pb.collection("news").create(
+        { title: body.title, image: body.image, content: body.message },
+        {
+          headers: { key: String(process.env.POCKETBASE_KEY) },
+          $autoCancel: false,
+        }
+      );
       return Response.json({ status: "OK" });
     } else {
       return Response.json({ status: "dinaxy" });
