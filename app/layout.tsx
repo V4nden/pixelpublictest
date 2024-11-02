4c459255d1ec9012d04d5b545447550a1765ee8e
V4nden <vanden.gd@gmail.com>
2024-11-02 20:29:23 +0300

    PIX-70: Sidebar instead of nav on mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,15 @@
 import type { Metadata } from "next";
-import { Inter, Roboto } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/src/widgets/NavBar/ui/NavBar";
-import { SessionProvider } from "next-auth/react";
 import AuthProvider from "@/src/app/auth/AuthProvider";
-import { ToastContainer } from "react-toastify";
 import ToastProvider from "@/src/shared/ui/Toasts/ToastProvider";
 import Footer from "@/src/widgets/Footer/ui/Footer";
-import localFont from "next/font/local";
 import getThreadsWithPlayer from "@/src/entities/Thread/api/getThreadsWithPlayer";
 import { getServerSession } from "next-auth";
 import { nextAuthOptions } from "@/src/app/auth/model/nextAuthOptions";
 import { IThread, IThreadExpandable } from "@/src/entities/Thread/model/types";
+import Sidebar from "@/src/widgets/Sidebar/Sidebar";
 const inter = Inter({ subsets: ["cyrillic"], weight: "400" });
 
 export const metadata: Metadata = {
@@ -29,10 +27,12 @@ export default async function RootLayout({
   const userThreads: IThread[] | undefined =
     session &&
     session.user.player &&
-    (await getThreadsWithPlayer(session.user.player, [
-      IThreadExpandable.RECENT_MESSAGE,
-      IThreadExpandable.CREATOR,
-    ]));
+    (
+      await getThreadsWithPlayer(session.user.player, [
+        IThreadExpandable.RECENT_MESSAGE,
+        IThreadExpandable.CREATOR,
+      ])
+    ).slice(0, 3);
 
   return (
     <html lang="en">
@@ -42,7 +42,8 @@ export default async function RootLayout({
       <body className={inter.className}>
         <AuthProvider>
           <ToastProvider>
-            <NavBar userThreads={userThreads?.slice(0, 3)} />
+            <NavBar userThreads={userThreads} />
+            <Sidebar userThreads={userThreads} />
             {children}
             <Footer />
           </ToastProvider>
